545b060b6db0b8b63c68e2188770be1b23fe3832
Diego Henrique <diego.hc.sampaio@gmail.com>
2026-02-20 13:16:42 -0300

    feat: add aba conferidos

--- components/itemsConferidos.tsx
+++ components/itemsConferidos.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { View, Text, Modal, Button } from "react-native";
 import { useEffect, useState } from "react";
 import { db } from "../services/database";
 const props = {
@@ -11,15 +11,46 @@ function ItemsConferidos({ conferencia, array, setArray }: props) {
     "SELECT produto FROM itens where conferencia = ?",
     [conferencia]
   );
+  const [modalVisible, setModalVisible] = useState(false);
   useEffect(() => {
     setArray(conferidos);
   }, [conferidos]);
 
   return (
-    <View>
+    <View style={{ backgroundColor: "black", flex: 1 }}>
+      <Modal
+        visible={modalVisible}
+        transparent
+        animationType="slide"
+        onRequestClose={() => setModalVisible(false)}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <View>
+              <Text style={styles.modalTitle}>Itens Conferidos</Text>
+              <View style={styles.modalView}>
+                {array.map((item, index) => (
+                  <View style={{ width: "45%" }} key={index}>
+                    <Button
+                      key={index}
+                      title={item.produto}
+                      color={"#1C314E"}
+                    />
+                  </View>
+                ))}
+              </View>
+            </View>
+            <View>
+              <Button title="Fechar" onPress={() => setModalVisible(false)} />
+            </View>
+          </View>
+        </View>
+      </Modal>
       {array.length > 0 ? (
         <View style={styles.bottomView}>
-          <Text style={styles.white}>Conferidos ({array.length})</Text>
+          <Text style={styles.white} onPress={() => setModalVisible(true)}>
+            Conferidos ({array.length})
+          </Text>
         </View>
       ) : (
         <View></View>
@@ -28,6 +59,44 @@ function ItemsConferidos({ conferencia, array, setArray }: props) {
   );
 }
 const styles = {
+  item: {
+    fontSize: 16,
+    marginBottom: 10,
+    padding: 10,
+  },
+  modalView: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 20,
+    color: "white",
+  },
+  modalContainer: {
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 20,
+    width: "95%",
+    maxHeight: "95%",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    alignSelf: "center",
+  },
+  modalContent: {
+    flex: 1,
+    justifyContent: "space-between",
+    padding: 20,
+    width: "100%",
+  },
+  modalItem: {
+    marginBottom: 10,
+  },
+  modalTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 15,
+  },
   bottomView: {
     position: "absolute",
     bottom: 0,
